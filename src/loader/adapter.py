409f90e441709ccc8000407dd5f827118455feb5
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-02-13 15:59:57 +0100

    Temporarily disabled SAV (SPSS) reader

--- src/loader/adapter.py
+++ src/loader/adapter.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import os
-os.environ["DYLD_LIBRARY_PATH"] = "../lib/python2.7/site-packages/savReaderWriter/spssio/macos"
+# TODO: Temporarily Disabled
+# import os
+# os.environ["DYLD_LIBRARY_PATH"] = "../lib/python2.7/site-packages/savReaderWriter/spssio/macos"
 
 from collections import OrderedDict, defaultdict
-from savReaderWriter import SavReader, SavHeaderReader
+# TODO: Temporarily Disabled
+# from savReaderWriter import SavReader, SavHeaderReader
 import csv
 
 
@@ -72,56 +74,56 @@ class Adapter(object):
             print "No header or no metadata present"
             return False
 
-
-class SavAdapter(Adapter):
-    
-    def __init__(self, dataset):
-        super(SavAdapter, self).__init__(dataset)
-        
-        if not dataset['format'] == 'SPSS':
-            raise Exception('This is an SPSS adapter, not {}'.format(dataset['format']))
-            
-        self.filename = dataset['filename']
-        
-        self.has_header = dataset['header']
-        
-        self.reader = SavReader(self.filename, ioLocale='en_US.UTF-8')
-        
-        if self.has_header:
-            with SavHeaderReader(self.filename, ioLocale='en_US.UTF-8') as hr:
-                self.header = hr.varNames
-            
-        else :
-            self.header = None
-            
-        self.metadata = self.load_metadata()
-        
-        print self.validate_header()
-        return
-    
-    def get_examples(self):
-        """Returns first 10000 rows, and converts it to samples for each column."""
-        
-        # Get first 10000 rows
-        rows = self.reader.head(10000)
-        
-        # Assume metadata keys are best (since if no metadata exists, the header will be used to generate it)
-        header = self.metadata.keys()
-        
-        # Convert the rows to a list of dictionaries with keys from the header
-        data_dictionaries = [dict(zip(header, [v.strip() if type(v) == str else v for v in values ])) for values in rows]
-        
-        # Convert the list of dictionaries to a dictionary of sets
-        data = defaultdict(set)
-        for d in data_dictionaries:
-            for k, v in d.items():
-                data[k].add(v)
-        
-        json_ready_data = {}
-        for k,v in data.items():
-            json_ready_data[k] = list(v)[:250]
-            
-        return json_ready_data
+# TODO: Temporarily Disabled
+# class SavAdapter(Adapter):
+#
+#     def __init__(self, dataset):
+#         super(SavAdapter, self).__init__(dataset)
+#
+#         if not dataset['format'] == 'SPSS':
+#             raise Exception('This is an SPSS adapter, not {}'.format(dataset['format']))
+#
+#         self.filename = dataset['filename']
+#
+#         self.has_header = dataset['header']
+#
+#         self.reader = SavReader(self.filename, ioLocale='en_US.UTF-8')
+#
+#         if self.has_header:
+#             with SavHeaderReader(self.filename, ioLocale='en_US.UTF-8') as hr:
+#                 self.header = hr.varNames
+#
+#         else :
+#             self.header = None
+#
+#         self.metadata = self.load_metadata()
+#
+#         print self.validate_header()
+#         return
+#
+#     def get_examples(self):
+#         """Returns first 10000 rows, and converts it to samples for each column."""
+#
+#         # Get first 10000 rows
+#         rows = self.reader.head(10000)
+#
+#         # Assume metadata keys are best (since if no metadata exists, the header will be used to generate it)
+#         header = self.metadata.keys()
+#
+#         # Convert the rows to a list of dictionaries with keys from the header
+#         data_dictionaries = [dict(zip(header, [v.strip() if type(v) == str else v for v in values ])) for values in rows]
+#
+#         # Convert the list of dictionaries to a dictionary of sets
+#         data = defaultdict(set)
+#         for d in data_dictionaries:
+#             for k, v in d.items():
+#                 data[k].add(v)
+#
+#         json_ready_data = {}
+#         for k,v in data.items():
+#             json_ready_data[k] = list(v)[:250]
+#
+#         return json_ready_data
     
 class CsvAdapter(Adapter):
     
@@ -149,9 +151,41 @@ class CsvAdapter(Adapter):
         print self.validate_header()
         return
         
+        
+    def get_examples(self):
+        """Returns first 10000 rows, and converts it to samples for each column."""
+        
+        # Get first 10000 rows
+        count = 0
+        rows = []
+        for row in self.reader:
+            if count == 10000 :
+                break
+            rows.append(row)
+            count += 1
+            
+        # rows = self.reader.head(10000)
+        
+        # Assume metadata keys are best (since if no metadata exists, the header will be used to generate it)
+        header = self.metadata.keys()
+        
+        # Convert the rows to a list of dictionaries with keys from the header
+        data_dictionaries = [dict(zip(header, [v.strip() if type(v) == str else v for v in values ])) for values in rows]
+        
+        # Convert the list of dictionaries to a dictionary of sets
+        data = defaultdict(set)
+        for d in data_dictionaries:
+            for k, v in d.items():
+                data[k].add(v)
+        
+        json_ready_data = {}
+        for k,v in data.items():
+            json_ready_data[k] = list(v)[:250]
+            
+        return json_ready_data
 
 mappings = {
-    "SPSS": SavAdapter,
+    # "SPSS": SavAdapter,
     "CSV": CsvAdapter
 }
 
