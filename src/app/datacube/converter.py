1871e8cd3ea4444c9062a4572c311279f75b0297
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-04-07 14:55:35 +0200

    Generating complete datacube dataset description RDF from the data in the form (yay!)

--- src/app/datacube/converter.py
+++ src/app/datacube/converter.py
@@ -1,13 +1,22 @@
 from rdflib import Graph, Namespace, Literal, URIRef, BNode, RDF, RDFS, OWL
-
+import urllib
 
 QBRV = Namespace('http://qber.data2semantics.org/vocab/')
 QBR = Namespace('http://qber.data2semantics.org/resource/')
 
 QB = Namespace('http://purl.org/linked-data/cube#')
+SKOS = Namespace('http://www.w3.org/2004/02/skos/core#')
+PROV = Namespace('http://www.w3.org/ns/prov#')
+
+def safe_url(NS, local):
+    safe_local = local.replace(' ','_')
+    return NS[safe_local]
+
 
 def data_structure_definition(dataset, variables):
+    
     BASE = Namespace('http://qber.data2semantics.org/resource/{}/'.format(dataset))
+
 
 
     g = Graph()
@@ -15,6 +24,8 @@ def data_structure_definition(dataset, variables):
     g.bind('qbrv',QBRV)
     g.bind('qbr',QBR)
     g.bind('qb',QB)
+    g.bind('skos',SKOS)
+    g.bind('prov',PROV)
     
     dataset_uri = QBR[dataset]
     structure_uri = BASE['structure']
@@ -26,57 +37,90 @@ def data_structure_definition(dataset, variables):
     
     for variable_id, metadata in variables.items():
         variable_uri = BASE[variable_id]
-        component_uri = BASE['component/'+variable_id]
+        component_uri = safe_url(BASE,'component/'+variable_id)
         
         g.add((structure_uri, QB['component'], component_uri))
         
         ### DIMENSION PROPERTIES
-        if 'description' in metadata:
-            g.add((variable_uri, RDFS.comment, Literal(metadata['description'])))
+        g.add((variable_uri, RDFS.label, Literal(variable_id)))
         
-        if 'dimension_map' in metadata and metadata['dimension_map'] == 'on' and 'uri_field' in metadata:
-            for parent_dimension in metadata['uri_field']:
-                g.add((variable_uri, RDFS.subPropertyOf, URIRef(parent_dimension)))
-                
-        if 'dimension_type' in metadata :
+        if metadata['description'] != "":
+            g.add((variable_uri, RDFS.comment, Literal(metadata['description'])))
+
+        if metadata['dimension_type'] != "" :
             dimension_type_uri = URIRef(metadata['dimension_type'])
             
-            g.add((variable_uri, RDF.type, dimension_type_uri))
-            
             if dimension_type_uri == QB['DimensionProperty']:
+                g.add((variable_uri, RDF.type, dimension_type_uri))
                 g.add((component_uri, QB['dimension'], variable_uri))
             elif dimension_type_uri == QB['MeasureProperty']:
+                g.add((variable_uri, RDF.type, dimension_type_uri))
                 g.add((component_uri, QB['measure'], variable_uri))
             elif dimension_type_uri == QB['AttributeProperty']:
+                g.add((variable_uri, RDF.type, dimension_type_uri))
                 g.add((component_uri, QB['attribute'], variable_uri))
+        elif metadata['lod_variable_field'] != "" or metadata['codelist_checkbox'] == True or metadata['learn_codelist_checkbox'] == True:
+            # It must be a dimension....
+            g.add((variable_uri, RDF.type, QB['DimensionProperty']))
+            g.add((component_uri, QB['dimension'], variable_uri))
+        else :
+            # Otherwise we assume it is just an attribute
+            g.add((variable_uri, RDF.type, QB['AttributeProperty']))
+            g.add((component_uri, QB['attribute'], variable_uri))
                 
-        ### DATA CONVERSION INSTRUCTIONS
-        data_adapters = {}
+
+
+        # If we have a variable from the LOD cloud
+        if metadata['lod_variable_field'] != "":
+            g.add((variable_uri, RDFS.subPropertyOf, URIRef(metadata['lod_variable_field'])))
         
-        if 'restrict_to_skos' in metadata and metadata['restrict_to_skos'] == 'on' and 'skos_field' in metadata:
+        
+        # If we have a codelist for this variable
+        if metadata['codelist_checkbox'] == True or metadata['learn_codelist_checkbox'] == True:
+            # The variable should have its own codelist (a collection, since we don't know the hierarchy)
+            codelist_uri = safe_url(BASE,'codelist/'+variable_id)
+            g.add((codelist_uri, RDF.type, SKOS['Collection']))
+            g.add((codelist_uri, RDFS.label, Literal('Code list for "{}" in dataset {}'.format(variable_id, dataset))))
+            
+            # The variable should be typed as a 'coded property'
             g.add((variable_uri, RDF.type, QB['CodedProperty']))
             
-            for skos_scheme in metadata['skos_field']:
-                g.add((variable_uri, QB['codeList'], URIRef(skos_scheme)))
+            # And it should point to the codelist
+            g.add((variable_uri, QB['codeList'], codelist_uri))
+
+            # Generate a SKOS concept for each of the values
+            for value in metadata['values']:
+                # Skip if the value is not long enough
+                if value['id'].strip() == '':
+                    continue
+                    
+                value_id = value['id']
+                value_uri = safe_url(BASE,'code/'+variable_id+'/'+value_id)
                 
-            adapter = {'transformer': concept_transformer, 'uri': variable_uri, 'base_ns': BASE}
-                
-        if 'skos_learn' in metadata and 'use_regex' in metadata and 'regex' in metadata:
-            regex = metadata['regex']
-            print regex
+                g.add((value_uri,RDF.type,SKOS['Concept']))
+                g.add((value_uri,SKOS['prefLabel'], Literal(value_id)))
+                g.add((codelist_uri,SKOS['member'], value_uri))
+
+            # If we have a mapping specified, map to the codelist we selected
+            if metadata['codelist_field'] != "":
+                g.add((codelist_uri, PROV['wasDerivedFrom'], URIRef(metadata['codelist_field'])))
             
-            
+                for mapping in metadata['mappings']:
+                    source = mapping['id']
+                    target = mapping['value']                
+
+                    g.add((safe_url(BASE,'code/'+variable_id+'/'+source),SKOS['exactMatch'], URIRef(target)))
                 
     return g
         
         
-def concept_transformer(label, uri='http://example.com/data/dimension', base_ns=Namespace('http://example.com/data/')):
-    g = Graph()
-    
-    g.bind('qbrv',QBRV)
-    g.bind('qbr',QBR)
-    g.bind('qb',QB)
-    g.bind('data',base_ns)
+# def concept_transformer(label, uri='http://example.com/data/dimension', base_ns=Namespace('http://example.com/data/')):
+#     g = Graph()
+#
+#     g.bind('qbrv',QBRV)
+#     g.bind('qbr',QBR)
+#     g.bind('qb',QB)
+#     g.bind('data',base_ns)
     
     
     
