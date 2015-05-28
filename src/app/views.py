92c6796a9cf1bf33f899e29ee19fa166fdcbb4ff
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-05-28 09:43:01 +0200

    Linted

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, g, request, jsonify, make_response, redirect, url_for, abort
+from flask import render_template, request, jsonify
 from flask.ext.socketio import emit
-from werkzeug.http import parse_accept_header
 import logging
 import requests
 import json
@@ -28,32 +27,38 @@ log.setLevel(logging.DEBUG)
 def index():
     return render_template('base.html')
 
+
 @app.route('/inspector')
 def inspector():
     return render_template('inspector.html')
+
 
 @app.route('/inspect')
 def inspect():
     data = util.inspector.update()
     return jsonify(data)
 
+
 @socketio.on('message', namespace='/inspector')
 def message(json):
-    log.debug('SocketIO message:\n'+ str(json))
+    log.debug('SocketIO message:\n' + str(json))
+
 
 @socketio.on('connect', namespace='/inspector')
 def connect():
     log.debug('SocketIO connected')
     emit('response', {'data': 'Connected'})
 
+
 @socketio.on('disconnect', namespace='/inspector')
 def disconnect():
     log.debug('SocketIO disconnected')
 
+
 @app.route('/metadata')
 def metadata():
     """Loads the metadata for a dataset specified by the 'file' relative path argument"""
-    dataset_file = request.args.get('file',False)
+    dataset_file = request.args.get('file', False)
 
     # Check whether a file has been provided
     if not dataset_file:
@@ -77,14 +82,15 @@ def metadata():
     variables = adapter.get_header()
     metadata = adapter.get_metadata()
     examples = adapter.get_examples()
-    short_metadata = {metadata.keys()[0]: metadata[metadata.keys()[0]]}
+
+    # Not used
+    # short_metadata = {metadata.keys()[0]: metadata[metadata.keys()[0]]}
 
     # Get the LSD dimensions from the LSD service (or a locally cached copy)
     # And concatenate it with the dimensions in the CSDH
     dimensions = get_lsd_dimensions() + get_csdh_dimensions()
     # Get all known SKOS schemes and collections from the LOD cache service
     schemes = get_schemes() + get_csdh_schemes()
-
 
     cache = read_cache(dataset_path)
 
@@ -103,7 +109,6 @@ def metadata():
         'cache': cache
     }
 
-
     return jsonify(data)
 
 
@@ -119,14 +124,15 @@ def read_cache(dataset_path):
     else :
         return {}
 
+
 def write_cache(dataset_path, data):
     dataset_cache_filename = "{}.cache.json".format(dataset_path)
 
-    with open(dataset_cache_filename,'w') as dataset_cache_file:
+    with open(dataset_cache_filename, 'w') as dataset_cache_file:
         json.dump(data, dataset_cache_file)
 
 
-@app.route('/menu',methods=['POST'])
+@app.route('/menu', methods=['POST'])
 def menu():
     """Render the menu for the items specified in the POST data (i.e. the variable names)"""
     req_json = request.get_json(force=True)
@@ -135,9 +141,10 @@ def menu():
     items = req_json['items']
     log.debug(items)
 
-    return render_template('menu.html',items=items)
+    return render_template('menu.html', items=items)
 
-@app.route('/variable/ui',methods=['POST'])
+
+@app.route('/variable/ui', methods=['POST'])
 def variable():
     """Takes the variable details from the POST data and returns the UI for editing"""
     req_json = request.get_json(force=True)
@@ -149,24 +156,27 @@ def variable():
 
     log.debug(examples)
 
-    return render_template('variable.html',id=variable_id,description=description,examples=examples)
+    return render_template('variable.html',
+                           id=variable_id,
+                           description=description,
+                           examples=examples)
 
 
-@app.route('/variable/resolve',methods=['GET'])
+@app.route('/variable/resolve', methods=['GET'])
 def dimension():
     """Resolves the URI of a variable and retrieves its definition"""
     uri = request.args.get('uri', False)
 
-    if uri :
+    if uri:
         exists = sc.sparql("""
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
             ASK {{<{}> rdfs:label ?l .}}""".format(uri))
 
-        if not "true" in exists:
+        if "true" not in exists:
             success, visited = sc.resolve(uri, depth=2)
             print "Resolved ", visited
-        else :
+        else:
             success = True
 
         if success:
@@ -227,7 +237,6 @@ def dimension():
 
             log.debug(codelist_results)
 
-
             if len(codelist_results) > 0 :
                 codelist = sc.dictize(codelist_results)
                 variable_definition['codelist'] = codelist
@@ -235,16 +244,14 @@ def dimension():
             log.debug(variable_definition)
             return jsonify(variable_definition)
 
-
-        else :
+        else:
             return 'error'
 
-
-    else :
+    else:
         return 'error'
 
 
-@app.route('/codelist/concepts',methods=['GET'])
+@app.route('/codelist/concepts', methods=['GET'])
 def codelist():
     """Gets the SKOS Concepts belonging to the SKOS Scheme or Collection identified by the URI parameter"""
     uri = request.args.get('uri', False)
@@ -282,7 +289,7 @@ def codelist():
             lod_codelist_results = sparql.query().convert()['results']['bindings']
             if len(lod_codelist_results) > 0:
                 lod_codelist = sc.dictize(lod_codelist_results)
-            else :
+            else:
                 lod_codelist = []
 
             log.debug(lod_codelist)
@@ -297,13 +304,10 @@ def codelist():
             sdh_codelist_results = sc.sparql(query)
             if len(sdh_codelist_results) > 0:
                 sdh_codelist = sc.dictize(sdh_codelist_results)
-            else :
+            else:
                 sdh_codelist = []
 
-
             log.debug(sdh_codelist)
-
-
 
         except Exception as e:
             log.error(e)
@@ -313,13 +317,11 @@ def codelist():
 
         if lod_codelist == [] and sdh_codelist == []:
             return jsonify({'response': 'error', 'message': str(e)})
-        else :
-            return jsonify({'codelist': lod_codelist + sdh_codelist })
+        else:
+            return jsonify({'codelist': lod_codelist + sdh_codelist})
 
 
-
-
-@app.route('/save',methods=['POST'])
+@app.route('/save', methods=['POST'])
 def save():
     """Uses the DataCube converter to convert the JSON representation of variables to RDF DataCube"""
 
@@ -328,9 +330,8 @@ def save():
     dataset = req_json['file']
     dataset_path = req_json['path']
     profile = req_json['profile']
-    
 
-    source_hash = git_client.add_file(dataset_path,profile['name'],profile['email'])
+    source_hash = git_client.add_file(dataset_path, profile['name'], profile['email'])
 
     print source_hash
 
@@ -339,7 +340,7 @@ def save():
     data = util.inspector.update(graph)
     socketio.emit('update', {'data': data}, namespace='/inspector')
 
-    with open('latest_update.ttl','w') as f:
+    with open('latest_update.ttl', 'w') as f:
         graph.serialize(f, format='turtle')
 
     query = sc.make_update(graph)
@@ -350,24 +351,18 @@ def save():
     return result
 
 
-
-
-
-@app.route('/browse',methods=['GET'])
+@app.route('/browse', methods=['GET'])
 def browse():
     """Takes a relative path, and returns a list of files/directories at that location as JSON"""
     path = request.args.get('path', None)
-    if not path :
-        raise Exception('Must specify a path!')
 
+    if not path:
+        raise Exception('Must specify a path!')
 
     log.debug('Will browse absolute path: {}/{}'.format(config.base_path, path))
     filelist, parent = fc.browse(config.base_path, path)
 
     return jsonify({'parent': parent, 'files': filelist})
-
-
-
 
 
 def get_lsd_dimensions():
@@ -376,25 +371,26 @@ def get_lsd_dimensions():
 
     if os.path.exists('metadata/dimensions.json'):
         log.debug("Loading dimensions from file...")
-        with open('metadata/dimensions.json','r') as f:
+        with open('metadata/dimensions.json', 'r') as f:
             dimensions_json = f.read()
 
         dimensions = json.loads(dimensions_json)
-    else :
+    else:
         dimensions_response = requests.get("http://amp.ops.few.vu.nl/data.json")
         log.debug("Loading dimensions from LSD service...")
-        try :
+        try:
             dimensions = json.loads(dimensions_response.content)
 
-            with open('metadata/dimensions.json','w') as f:
+            with open('metadata/dimensions.json', 'w') as f:
                 f.write(dimensions_response)
 
-        except :
+        except:
             log.error("Dimensions could not be loaded from service...")
 
             dimensions = []
 
     return dimensions
+
 
 def get_csdh_dimensions():
     """Loads the list of Linked Statistical Data dimensions (variables) from the CSDH"""
@@ -426,10 +422,11 @@ def get_csdh_dimensions():
     sdh_dimensions_results = sc.sparql(query)
     if len(sdh_dimensions_results) > 0:
         sdh_dimensions = sc.dictize(sdh_dimensions_results)
-    else :
+    else:
         sdh_dimensions = []
 
     return sdh_dimensions
+
 
 def get_csdh_schemes():
     """Loads SKOS Schemes (code lists) from the CSDH"""
@@ -462,17 +459,18 @@ def get_csdh_schemes():
 
     return schemes
 
+
 def get_schemes():
     """Loads SKOS Schemes (code lists) either from the LOD Cache, or from a cached copy"""
     if os.path.exists('metadata/schemes.json'):
         # TODO: Check the age of this file, and update if older than e.g. a week.
         log.debug("Loading schemes from file...")
-        with open('metadata/schemes.json','r') as f:
+        with open('metadata/schemes.json', 'r') as f:
             schemes_json = f.read()
 
         schemes = json.loads(schemes_json)
         return schemes
-    else :
+    else:
         log.debug("Loading schemes from RDF sources...")
         schemes = []
 
@@ -539,7 +537,7 @@ def get_schemes():
 
         schemes_json = json.dumps(schemes)
 
-        with open('metadata/schemes.json','w') as f:
+        with open('metadata/schemes.json', 'w') as f:
             f.write(schemes_json)
 
         return schemes
