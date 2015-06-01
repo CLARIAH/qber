9558afb9478be552ad5c2368b1b38dce4eb3fda4
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-06-01 16:18:19 +0200

    New gevent.subprocess based git hash collection that actually works (but no check on the output yet!... i.e. if git_command.py does not return anything sensible, then the whole thing may just crash)

--- src/app/util/git_client.py
+++ src/app/util/git_client.py
@@ -1,45 +1,10 @@
 import app.config as config
-from app import app
-import sh
-import os
-import logging
+import gevent.subprocess as sp
 
 DATA_PATH = config.base_path
 
-log = app.logger
-log.setLevel(logging.DEBUG)
-
-def get_repository():
-    log.debug("Baking git for {}".format(DATA_PATH))
-    git = sh.git.bake(_cwd=DATA_PATH)
-
-    try:
-        log.debug("Get the status")
-        git.status('.')
-    except:
-        log.debug("No status, no git repo")
-        git.init('.')
-
-    return git
-
 
 def add_file(absolute_path, author, email):
-    git = get_repository()
+    out = sp.check_output(['python','app/util/git_command.py',DATA_PATH, absolute_path, author, email])
 
-    # Get the relative path
-    path = os.path.relpath(absolute_path, DATA_PATH)
-
-    try:
-        log.debug("Adding {}".format(path))
-        git.add(path)
-        log.debug("Added {}".format(path))
-        log.debug("Committing")
-        git.commit('--author=\"{} <{}>\"'.format(author, email), m="QBer commit by {} (<{}>)".format(author, email))
-        log.debug("Committed")
-    except Exception as e:
-        log.warning("Nothing added (nothing changed?)")
-
-    log.debug("Calling git ls-files to get sha hash for file {}".format(path))
-    sha_hash = git('ls-files', '-s', absolute_path).split(' ')[1]
-    log.debug("Got hash {}".format(sha_hash))
-    return sha_hash
+    return out.strip()
