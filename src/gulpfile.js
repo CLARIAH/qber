9ae7788458a914bd7296f5483435b18879ea6e15
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-06-11 16:44:40 +0200

    Bower, NPM, Gulp package management stuff

---
+++ src/gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// requirements
+
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    size = require('gulp-size'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean');
+
+
+// tasks
+
+gulp.task('transform', function () {
+  // add task
+});
+
+gulp.task('default', ['clean'], function () {
+  console.log("Hello QBer!");
+  gulp.start('transform');
+  gulp.watch('./app/static/jsx/main.js', ['transform']);
+});
+
+
+
+
+gulp.task('transform', function () {
+  return gulp.src('./app/static/jsx/qber.jsx')
+    .pipe(browserify({transform: ['reactify']}))
+    .pipe(rename('qber.js'))
+    .pipe(gulp.dest('./app/static/js'))
+    .pipe(size());
+});
+
+
+gulp.task('clean', function () {
+  return gulp.src(['./app/static/js'], {read: false})
+    .pipe(clean());
+});
---
+++ src/install.sh
@@ -0,0 +1,6 @@
+echo "Installing dependencies"
+echo "Requirements: pip, npm and bower"
+echo "Installation will fail if one of these is not present on your system"
+pip install -r requirements.txt
+npm install
+bower install
