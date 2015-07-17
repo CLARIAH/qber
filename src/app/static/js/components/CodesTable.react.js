ff60caf623df0f5336fa02fbc170caed7ea3aac4
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-07-17 15:40:19 +0200

    Show/hide codes table

--- src/app/static/js/components/CodesTable.react.js
+++ src/app/static/js/components/CodesTable.react.js
@@ -1,12 +1,19 @@
 var React = require('react');
 var ReactPropTypes = React.PropTypes;
-var DatasetActions = require('../actions/DatasetActions');
+var PillSelector = require('./PillSelector.react');
+
 
 var CodesTable = React.createClass({
 
   // This React class only works if a list of 'codes' is passed through its properties.
   propTypes: {
     codes: ReactPropTypes.array.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      'visible': true
+    };
   },
 
   /**
@@ -19,23 +26,55 @@ var CodesTable = React.createClass({
       return null;
     }
 
-    var codes = this.props.codes;
-    var codes_rows = [];
-
-    for (var key in codes) {
-      codes_rows.push(<tr key={codes[key].id}><td> { codes[key].id } <span className='badge pull-right'> { codes[key].count }</span></td></tr>);
+    var caret;
+    if (this.state.visible){
+      caret = <span className="small glyphicon glyphicon-chevron-up"></span>;
+    } else {
+      caret = <span className="small glyphicon glyphicon-chevron-down"></span>;
     }
 
+    var table;
+    if (this.state.visible) {
+      var codes = this.props.codes;
+      var codes_rows = [];
+
+      for (var key in codes) {
+        codes_rows.push(<tr key={codes[key].id}>
+                          <td> { codes[key].id } </td>
+                          <td> <span className='badge pull-right'> { codes[key].count }</span></td></tr>);
+      }
+
+      table = <div style={{overflow: 'scroll', maxHeight: '300px'}}>
+                <table className="table table-striped">
+                  <tbody>
+                    {codes_rows}
+                  </tbody>
+                </table>
+              </div>;
+    }
+
+
+
     return (
-      <div style={{overflow: 'scroll', height: '200px'}}>
-        <table className="table table-striped">
-          <tbody>
-            {codes_rows}
-          </tbody>
-        </table>
-      </div>
+      <section id="codes_table">
+        <h4 onClick={this._onToggle} aria-expanded={this.state.visible}>Frequency Table {caret}</h4>
+        {table}
+      </section>
     );
-  }
+  },
+
+  _filter: function(option){
+    return (option.label.search(regexp) > -1) ? '': (option.uri.search(regexp) > -1) ? '': 'none';
+  },
+
+  /**
+   * Event handler for the visible/invisible click
+   */
+  _onToggle: function() {
+    var new_state = this.state;
+    new_state.visible = !this.state.visible;
+    this.setState(new_state);
+  },
 });
 
 module.exports = CodesTable;
