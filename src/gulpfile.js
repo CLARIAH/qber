5c2d1add92ffb70a20f58013b0508b9eb904c14b
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-03 14:57:03 +0100

    Fixed paths

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -18,22 +18,22 @@ gulp.task('transform', function () {
 gulp.task('default', ['clean'], function () {
   console.log("Hello QBer!");
   gulp.start('transform');
-  gulp.watch(['./app/static/js/qber.js','./app/static/js/**/*.js'], ['transform']);
+  gulp.watch(['./js/qber.js','./js/**/*.js'], ['transform']);
 });
 
 
 
 
 gulp.task('transform', function () {
-  return gulp.src('./app/static/js/qber.js')
+  return gulp.src('./js/qber.js')
     .pipe(browserify({transform: ['reactify']}))
     .pipe(rename('bundle.js'))
-    .pipe(gulp.dest('./app/static/dist/js'))
+    .pipe(gulp.dest('./dist/js'))
     .pipe(size());
 });
 
 
 gulp.task('clean', function () {
-  return gulp.src(['./app/static/dist/js'], {read: false})
+  return gulp.src(['./dist/js'], {read: false})
     .pipe(clean());
 });
