22bbc2d254440d76ff0b1455fd412cf30c0668e8
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-03 14:55:46 +0100

    Removed all API related files

--- src/convert.py
+++
@@ -1,4 +0,0 @@
-import loader.reader
-
-
-loader.reader.go('loader/canada.json')
--- stardog_uploader/synchronize_dropbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Downloading SDH RDF files from Dropbox"
-wget -O rdf.zip https://www.dropbox.com/sh/2lz1zwgtuvt7kwc/AAASZIMIks6qgp1OpI-ZO5X9a?dl=1
-echo "Done"
-echo "Unzipping..."
-unzip rdf.zip
-echo "Done"
