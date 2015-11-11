eb63246efa9ffdd9a29ea3fb718f30e8a970d94c
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-11 13:50:09 +0100

    Correct dealing with responses, renamed retrieveCodes to retrieveConcepts

--- src/js/utils/QBerAPI.js
+++ src/js/utils/QBerAPI.js
@@ -34,64 +34,47 @@ module.exports = {
     $.post(CSDH_API + '/dataset/save',data=JSON.stringify({'dataset': settings.dataset}), function(response){
       console.log(response);
       settings.success(response);
-    }).fail(function(){
+    }).fail(function(response){
       settings.error(response);
     });
   },
 
   retrieveDimension: function(settings) {
-    $.get(CSDH_API + '/community/definition', {'uri': settings.dimension}, function(dimension_details){
+    $.get(CSDH_API + '/community/definition', {'uri': settings.dimension}, function(response){
       console.log("QBerAPI retrieveDimension");
-      console.log(dimension_details);
-      if(dimension_details == 'error'){
-        settings.error(settings.dimension);
-      } else {
-        settings.success(dimension_details);
-      }
-    }).fail(function(){
-      settings.error(settings.dimension);
+      console.log(response);
+      settings.success(response);
+    }).fail(function(response){
+      settings.error(response);
     });
   },
 
-  retrieveCodes: function(settings) {
-    $.get(CSDH_API + '/community/concepts', {'uri': settings.codelist_uri}, function(codes){
-      console.log("QBerAPI retrieveCodes");
-      console.log(codes);
-      if(codes.response && codes.response == 'error'){
-        settings.error(settings.codelist_uri);
-      } else {
-        settings.success(codes.codelist);
-      }
-    }).fail(function(){
-      settings.error(settings.codelist_uri);
+  retrieveConcepts: function(settings) {
+    $.get(CSDH_API + '/community/concepts', {'uri': settings.scheme_uri}, function(response){
+      console.log("QBerAPI retrieveConcepts");
+      settings.success(response);
+    }).fail(function(response){
+      settings.error(response);
     });
   },
 
   retrieveFileList: function(settings) {
-    $.get(CSDH_API + '/browse', {'path': settings.path}, function(data){
+    $.get(CSDH_API + '/browse', {'path': settings.path}, function(response){
       console.log("QBerAPI retrieveFileList");
-      console.log(data);
-      if(data.response == 'error'){
-        settings.error(settings.path);
-      } else {
-        settings.success(data);
-      }
-    }).fail(function(){
-      settings.error(settings.path);
+      console.log(response);
+      settings.success(response);
+    }).fail(function(response){
+      settings.error(response);
     });
   },
 
   retrieveIRI: function(settings) {
-    $.get(CSDH_API + '/iri', {'iri': settings.iri}, function(data){
+    $.get(CSDH_API + '/iri', {'iri': settings.iri}, function(response){
       console.log("QBerAPI retrieveIRI");
-      console.log(data);
-      if(data.response == 'error'){
-        settings.error(settings.iri);
-      } else {
-        settings.success(data.response);
-      }
-    }).fail(function(){
-      settings.error(settings.iri);
+      console.log(response);
+      settings.success(response);
+    }).fail(function(response){
+      settings.error(response);
     });
   }
 };
