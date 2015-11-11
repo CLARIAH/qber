8a6269e00eb83a6faed810cbe07f7183bd42abf9
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-11 13:52:21 +0100

    Initialize store when loading the page

--- src/js/qber.js
+++ src/js/qber.js
@@ -2,6 +2,9 @@ var React = require('react');
 var QBer = require('./components/QBer.react');
 var MessagePanel = require('./components/MessagePanel.react');
 
+var DatasetActions = require('./actions/DatasetActions');
+
+DatasetActions.initializeStore();
 
 React.render(
   <QBer />,
