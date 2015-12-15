460336c7e20c584807fcdd699cccf8a62b2d69f5
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-12-15 10:10:45 +0100

    Removed old references to python and bower stuff

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,4 @@
 echo "Installing dependencies"
-echo "Requirements: pip, npm and bower"
+echo "Requirements: npm"
 echo "Installation will fail if one of these is not present on your system"
-pip install -r ../requirements.txt
 npm install
-bower install
